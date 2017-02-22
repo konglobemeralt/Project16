81531a0383ad87e86717ad5d15566d7663d7db29
Konglobemeralt <jesper.blidkvist@live.se>
2017-02-22 09:51:49 +0100

    Tied iMatController to fxml, toolbar buttons and two top menue buttons debug prints to notify that pressing them works

--- src/sample/iMatController.java
+++ src/sample/iMatController.java
@@ -1,6 +1,32 @@
 package sample;
 
+import java.awt.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
 public class iMatController {
+    private Button helpButton;
 
+   public void helpButtonClicked(ActionEvent event){
+       System.out.println("Help");
+   }
+   public void profileButtonClicked(ActionEvent event){
+        System.out.println("profile");
+   }
 
+    public void kundkorgButtonClicked(ActionEvent event){
+        System.out.println("kundkorg");
+    }
+
+    public void minaListorButtonClicked(ActionEvent event){
+        System.out.println("minaListor");
+    }
+
+    public void gamlaKvittonButtonClicked(ActionEvent event){
+        System.out.println("gamlaListor");
+    }
 }
