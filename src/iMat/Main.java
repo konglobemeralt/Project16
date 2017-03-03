e63807460dba554191e2d39110ac801f333fec99
Konglobemeralt <jesper.blidkvist@live.se>
2017-03-03 12:41:12 +0100

    Change color scheme for buttons

--- src/iMat/Main.java
+++ src/iMat/Main.java
@@ -34,7 +34,7 @@ public class Main extends Application {
         this.primaryStage.setResizable(false);
         showMainView();
         showCategoriesView();
-        //showProductView();
+        showProductView();
 
 
         //Test
